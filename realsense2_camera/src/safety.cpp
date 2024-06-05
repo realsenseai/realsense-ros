426e494f32a3d524ce0c5c75f71e68eeeb1e4380
SamerKhshiboun <samer.khshiboun@intel.com>
2024-06-05 05:09:59 +0000

    support sic read/write services

--- realsense2_camera/src/safety.cpp
+++ realsense2_camera/src/safety.cpp
@@ -41,6 +41,19 @@ void BaseRealSenseNode::publishSafetyServices()
             [&](const realsense2_camera_msgs::srv::SafetyPresetWrite::Request::SharedPtr req,
                         realsense2_camera_msgs::srv::SafetyPresetWrite::Response::SharedPtr res)
                         {SafetyPresetWriteService(req, res);});
+
+    _safety_interface_config_read_srv = _node.create_service<realsense2_camera_msgs::srv::SafetyInterfaceConfigRead>(
+            "~/safety_interface_config_read",
+            [&](const realsense2_camera_msgs::srv::SafetyInterfaceConfigRead::Request::SharedPtr req,
+                        realsense2_camera_msgs::srv::SafetyInterfaceConfigRead::Response::SharedPtr res)
+                        {SafetyInterfaceConfigReadService(req, res);});
+
+    _safety_interface_config_write_srv = _node.create_service<realsense2_camera_msgs::srv::SafetyInterfaceConfigWrite>(
+            "~/safety_interface_config_write",
+            [&](const realsense2_camera_msgs::srv::SafetyInterfaceConfigWrite::Request::SharedPtr req,
+                        realsense2_camera_msgs::srv::SafetyInterfaceConfigWrite::Response::SharedPtr res)
+                        {SafetyInterfaceConfigWriteService(req, res);});
+
 }
 
 void BaseRealSenseNode::SafetyPresetReadService(const realsense2_camera_msgs::srv::SafetyPresetRead::Request::SharedPtr req,
@@ -64,6 +77,37 @@ void BaseRealSenseNode::SafetyPresetWriteService(const realsense2_camera_msgs::s
     {
         rs2_safety_preset sp = _safety_sensor->as<rs2::safety_sensor>().json_string_to_safety_preset(req->preset);
         _safety_sensor->as<rs2::safety_sensor>().set_safety_preset(req->index, sp);
+        res->success = true;
+    }
+    catch (const std::exception &e)
+    {
+        res->success = false;
+        res->error_message = std::string("Exception occurred: ") + e.what();
+    }
+}
+
+void BaseRealSenseNode::SafetyInterfaceConfigReadService(const realsense2_camera_msgs::srv::SafetyInterfaceConfigRead::Request::SharedPtr req,
+    realsense2_camera_msgs::srv::SafetyInterfaceConfigRead::Response::SharedPtr res){
+    try
+    {
+        rs2_calib_location location = static_cast<rs2_calib_location>(req->calib_location);
+        rs2_safety_interface_config sic = _safety_sensor->as<rs2::safety_sensor>().get_safety_interface_config(location);
+        res->safety_interface_config= std::string(_safety_sensor->as<rs2::safety_sensor>().safety_interface_config_to_json_string(sic));
+        res->success = true;
+    }
+    catch (const std::exception &e)
+    {
+        res->success = false;
+        res->error_message = std::string("Exception occurred: ") + e.what();
+    }
+}
+
+void BaseRealSenseNode::SafetyInterfaceConfigWriteService(const realsense2_camera_msgs::srv::SafetyInterfaceConfigWrite::Request::SharedPtr req,
+    realsense2_camera_msgs::srv::SafetyInterfaceConfigWrite::Response::SharedPtr res){
+    try
+    {
+        rs2_safety_interface_config sic = _safety_sensor->as<rs2::safety_sensor>().json_string_to_safety_interface_config(req->safety_interface_config);
+        _safety_sensor->as<rs2::safety_sensor>().set_safety_interface_config(sic);
         res->success = true;
     }
     catch (const std::exception &e)
