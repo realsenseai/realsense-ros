39293dfc49c1e26d0b7b86b36483a7a2a7efdd52
PrasRsRos <prasrs.ros@gmail.com>
2024-07-30 11:03:42 +0530

    triggered calibration - intermediate checkin

--- realsense2_camera/test/d500/test_dryrun_triggered_calibration.py
+++ realsense2_camera/test/d500/test_dryrun_triggered_calibration.py
@@ -1,30 +1,16 @@
-'''
-from realsense2_camera_msgs.action import TriggeredCalibration
-from rclpy.action import ActionClient
-import rclpy
-from rclpy.node import Node
-#from rclpy.parameter import Parameter
-from rcl_interfaces.msg import Parameter
-from rcl_interfaces.msg import ParameterValue
-from rcl_interfaces.srv import SetParameters, GetParameters, ListParameters
-'''
-
-'''
-humble doesn't have the SetParametersResult and SetParameters_Response imported using 
-__init__.py. The below two lines can be used for iron and hopefully succeeding ROS2 versions
-'''
-
-
-'''
-
-#from rcl_interfaces.msg import SetParametersResult
-#from rcl_interfaces.srv import SetParameters_Response
-from rcl_interfaces.msg._set_parameters_result import SetParametersResult
-from rcl_interfaces.srv._set_parameters  import SetParameters_Response
-
-from rcl_interfaces.msg import ParameterType
-from rcl_interfaces.msg import ParameterValue
-'''
+# Copyright 2024 Intel Corporation. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 import sys
