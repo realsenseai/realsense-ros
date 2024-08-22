9123656f6390f4da43631e754de29c7aa5dba0f0
PrasRsRos <pras.rs.ros@gmail.com>
2024-08-22 16:00:13 +0530

    Added device info system tests

--- realsense2_ros_mqtt_bridge/test/utils/camera_n_mqtt_nodes.py
+++ realsense2_ros_mqtt_bridge/test/utils/camera_n_mqtt_nodes.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Intel Corporation. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import sys
 import time
@@ -173,7 +187,7 @@ def get_rs_node_description(params):
     comment out the '#prefix' line, if you like gdb and want to debug the code, you may have to do more
     if you have more than one rs node.
     '''
-    
+
     return launch_ros.actions.Node(
         package='realsense2_camera',
         namespace=params["camera_namespace"],
